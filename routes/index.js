e59076535fdfd1bb0b1918c663265e276183d93f
Valentin Vassas <valentinvassas@gmail.com>
2018-11-15 11:03:07 +0100

    route save

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//save tweets from py
+router.post('/save', function(req, res) {
+    var word = req.body.word;
+    var datetime = req.body.datetime;
+    if((typeof word != 'undefined') || (typeof datetime != 'undefined')){
+      var json = {
+        "word" : word,
+        "datetime" : datetime
+      }
+      MongoClient.connect('mongodb://pipibigdata:pipipipi1@ds163103.mlab.com:63103/epfbigdata', (err, client) => {
+        db = client.db('epfbigdata');
+        db.collection('tweets_of_the_day').save(json, (err, result) => {
+          if (err) return console.log(err);
+          console.log('saved to database');
+        })
+      });
+      res.send("ok");
+    } else {
+      res.send("ko");
+    }
 });
 
 module.exports = router;
